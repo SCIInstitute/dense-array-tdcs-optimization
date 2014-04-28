dc5e9e0b2ba49f80a53e46ce1a1f932d5745f9c9
Seyhmus <seyhmusguler@gmail.com>
2014-04-27 23:04:13 -0400

    deleted a function. Updated element center finder.

--- SurfaceNormal/elemCenter.m
+++ SurfaceNormal/elemCenter.m
@@ -1,7 +1,8 @@
 function centers = elemCenter(node,elem)
 %%Finds the centers of the elements by taking average of the
-%nodes belonging to same element.
-%Written by Seyhmus Guler, 3/8/14
+%node positions belonging to same element.
+%Written by Seyhmus Guler, 3/8/14 Last edit:4/27/14.
+
 %INPUTS:
 %node: Node positions. Size: #dimension x #nodes
 %elem: Elements. Size: #nodes per element x # elements
@@ -11,9 +12,8 @@ function centers = elemCenter(node,elem)
 [nD,~] = size(node); %node dimension (nD) and number of nodes (~)
 [eD,M] = size(elem); %element dimension (eD) and number of elements (M)
 
-expandedNode = zeros(eD,nD*M);
+expandedCenters = zeros(1,nD*M);
 for i=1:eD
-    expandedNode(i,:) = reshape(node(:,elem(i,:)),1,[]);
+    expandedCenters = expandedCenters + reshape(node(:,elem(i,:)),1,[]);
 end
-
-centers = reshape(sum(expandedNode)/eD,nD,[]);
+centers = reshape(expandedCenters/eD,nD,[]);
