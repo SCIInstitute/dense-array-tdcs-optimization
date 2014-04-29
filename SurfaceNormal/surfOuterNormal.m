5ac3ee9c4e6de2d3f95bac7542dde3f9956334c0
Seyhmus <seyhmusguler@gmail.com>
2014-04-29 18:53:09 -0400

    Surface normal functions commented.

--- SurfaceNormal/surfOuterNormal.m
+++ SurfaceNormal/surfOuterNormal.m
@@ -1,23 +1,38 @@
 function [surfNormal, surfElements] = surfOuterNormal(elem,node,volSurface)
-%Finds the outer surface normal on the surface of a mesh. 
-%Also finds surface element centers and indices for the surface triangles to be used as 
-%nodes for surface normal mesh.
+%Finds the outer surface normal on the surface of a mesh.
+%Also finds surface triangle element centers and indices to be used as
+%nodes for surface normal direction field.
 %elem: 4xM, node: 3xN
+
+%if surface elements are not defined, find them using iso2mesh toolbox
+%volface func
 if nargin < 3
-volSurface = volface(elem');
+    volSurface = volface(elem');
 end
 
+%We find the normal direction by cross product:
+%Normal of a triangle is the cross of its two edges, we will use edges
+%(vectors) 12 and 13.
 nodeDifference21 = node(:,volSurface(:,2))-node(:,volSurface(:,1));
 nodeDifference31 = node(:,volSurface(:,3))-node(:,volSurface(:,1));
-volElementIdx = zeros(1,size(volSurface,1));
+volElementIdx = zeros(1, size(volSurface,1));
 crossProduct = zeros(3, size(volSurface,1));
-for i=1:size(volSurface,1)    
-volElementIdx(i) = find(sum(ismember(elem,volSurface(i,:))) >= 3);
-crossProduct(:,i) = cross(nodeDifference21(:,i),nodeDifference31(:,i));
-crossProduct(:,i) = crossProduct(:,i)/norm(crossProduct(:,i));
+
+%For each surface triangle find the normal direction via for loop. There
+%may be computationally more efficient ways.
+for i=1:size(volSurface,1)
+    %find which cell that triangle belongs to
+    volElementIdx(i) = find(sum(ismember(elem,volSurface(i,:))) >= 3);
+    %cross producto to find the normal direction and normalization.
+    crossProduct(:,i) = cross(nodeDifference21(:,i),nodeDifference31(:,i));
+    crossProduct(:,i) = crossProduct(:,i)/norm(crossProduct(:,i));
 end
-surfElemC = elemCenter(node,elem(:,volElementIdx));
-surfTriC = elemCenter(node,volSurface');
-surfNormal = crossProduct .* repmat(sign(-sum(crossProduct .* (surfElemC-surfTriC))),3,1);
+
+%We will need to make sure the direction is outwards by following script.
+surfElemC = elemCenter(node,elem(:,volElementIdx)); %tetra centers.
+surfTriC = elemCenter(node,volSurface'); %triangle centers.
+
+%Convert the direction if it is inwards. 
+surfNormal = crossProduct .* repmat(sign(sum(crossProduct .* (surfTriC-surfElemC))),3,1);
 surfElements.elemCenters = surfTriC;
-surfElements.elemIndices = volElementIdx; 
+surfElements.elemIndices = volElementIdx;
--- SurfaceNormal/surfaceNormalProjection.m
+++ SurfaceNormal/surfaceNormalProjection.m
@@ -1,4 +1,4 @@
-function surfaceNormalD = surfaceNormalProjection(headMesh,roi,i)
+function surfaceNormalD = surfaceNormalProjection(headMesh,roi)
 %%FINDS SURFACE NORMAL BASED ON THE PROJECTION TO THE SURFACE
 %Written by: Seyhmus Guler, 3/8/14
 % INPUTS
@@ -21,20 +21,24 @@ roiSurface = volface(roiElements');
 
 
 %%Defining the surfaces to which the elements will be projected
+%We find CSF and WM touching of the ROI.
 roiCSFtouching = find(sum(ismember(roiSurface',elem(:,field==3))) >= 3);
 roiWMtouching = find(sum(ismember(roiSurface',elem(:,field==5))) >= 3);
 
-csfSurface.face = roiSurface(roiCSFtouching,:)';
-csfSurface.node = node;
-save(['csfSurfaceProjection' num2str(i)],'csfSurface');
-wmSurface.face = roiSurface(roiWMtouching,:)';
-wmSurface.node = node;
-save(['wmSurfaceProjection' num2str(i)],'wmSurface');
+% Save surface elements if needed. 
+% csfSurface.face = roiSurface(roiCSFtouching,:)';
+% csfSurface.node = node;
+% save('csfSurfaceProjection','csfSurface');
+% wmSurface.face = roiSurface(roiWMtouching,:)';
+% wmSurface.node = node;
+% save('wmSurfaceProjection','wmSurface');
 
 %%Define surface normals for the surface elements
 [rSONfield,rSONelements] = surfOuterNormal(roiElements,node,roiSurface);
 rSONcenters = rSONelements.elemCenters;
+%Keep only normals that are on the CSF or WM boundary.
 rSONfield = rSONfield(:,[roiCSFtouching roiWMtouching]);
+%Convert the direction for the CSF-ROI interface.
 rSONfield(:,1:numel(roiCSFtouching)) = rSONfield(:,1:numel(roiCSFtouching))*-1;
 rSONcenters = rSONcenters(:,[roiCSFtouching roiWMtouching]);
 
@@ -42,6 +46,8 @@ rSONcenters = rSONcenters(:,[roiCSFtouching roiWMtouching]);
 elementCenters = elemCenter(node,roiElements);
 idx = knnsearch(rSONcenters',elementCenters');
 
+%For each surface element, find the closest elements in the ROI and assign
+%them with the same normal direction. 
 for i=1:size(rSONcenters,2)
     surfaceNormalD(:,idx == i) = repmat(rSONfield(:,i),1,nnz(idx==i));
 end
