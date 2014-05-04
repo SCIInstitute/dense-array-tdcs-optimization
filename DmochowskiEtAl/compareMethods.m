e68a3bcc8128246333c0bd6dc77ef71eaa83b1f7
seyhmusCSL <seyhmusguler@gmail.com>
2014-05-04 19:23:28 -0400

    additional stats added to comparison.

--- DmochowskiEtAl/compareMethods.m
+++ DmochowskiEtAl/compareMethods.m
@@ -3,11 +3,12 @@ function [theirs,ours,stats] = compareMethods(sqrtQ_weighted, v_weighted, ind, w
 [theirs.Solution, theirs.Fval, theirs.tdV] = weightedLeastSquaresL1ConstraintByParraEtAl(sqrtQ_weighted,v_weighted,ind);
 
 pBrain = norm(cell2mat(sqrtQ)*theirs.Solution)^2;
-tot = (norm(theirs.Solution,1)+abs(sum(theirs.Solution)));
+tot = (norm(theirs.Solution,1)+abs(sum(theirs.Solution)))/2;
 wScale = norm(w);
-
+stats.pBrain = pBrain;
+stats.tTot = tot;
 [ours.Solution, ours.Fval, ours.dV] = optimizationUsingCvxToolbox(w/wScale,sqrtQ,tot,ind,pBrain);
-
+stats.oTot = ( norm(ours.Solution,1) + abs(sum(ours.Solution))) /2;
 ours.Fval= ours.Fval*wScale;
 dVect = G * (T*ours.Solution);
 currentIntensity = sqrt(sum(reshape(dVect.*dVect,3,[])));
