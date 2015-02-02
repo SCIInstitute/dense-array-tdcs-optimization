8a9f2cf414543e59971b8683f81f149b92e32908
Seyhmus <seyhmusguler@gmail.com>
2015-02-02 18:59:38 -0500

    Surface outer normal update

--- SurfaceNormal/surfOuterNormal.m
+++ SurfaceNormal/surfOuterNormal.m
@@ -1,44 +1,49 @@
-function [surfNormal, surfElements] = surfOuterNormal(elem,node,volSurface)
+function [surfNormal, surfElements] = surfOuterNormal(elem,node)
 %Finds the outer surface normal on the surface of a mesh.
 %Also finds surface triangle element centers and indices to be used as
 %nodes for surface normal direction field.
-%elem: 4xM, node: 3xN
+%elem: 4xM, node: 3xN,
 
 %if surface elements are not defined, find them using iso2mesh toolbox
 %volface func
-if nargin < 3
-    volSurface = volface(elem');
+
+volSurface = volface(elem')';
+permutations = perms(1:4);
+permutations(:,end) = [];
+mapping = zeros(size(volSurface,2),1);
+for i =1:size(permuations,1)
+    [~,lcb] = ismember(volSurface',elem(permutations(i,:),:)','rows');
+    mapping = mapping + lcb;
+end
+
+if(numel(mapping) ~= size(volSurface,2))
+    error('mapping and surface sizes dont match');
 end
 
 %We find the normal direction by cross product:
 %Normal of a triangle is the cross of its two edges, we will use edges
 %(vectors) 12 and 13.
-nodeDifference21 = node(:,volSurface(:,2))-node(:,volSurface(:,1));
-nodeDifference31 = node(:,volSurface(:,3))-node(:,volSurface(:,1));
-volElementIdx = zeros(1, size(volSurface,1));
-crossProduct = zeros(3, size(volSurface,1));
+u = node(:,volSurface(2,:))-node(:,volSurface(1,:));
+v = node(:,volSurface(3,:))-node(:,volSurface(1,:));
 
-%For each surface triangle find the normal direction via for loop. There
-%may be computationally more efficient ways.
-matlabpool close force
-matlabpool open;
-parfor i=1:size(volSurface,1)
-    %find which cell that triangle belongs to
-    volElementIdx(i) = find(sum(ismember(elem,volSurface(i,:))) >= 3);
-    %cross producto to find the normal direction and normalization.
-    crossProduct(:,i) = cross(nodeDifference21(:,i),nodeDifference31(:,i));
-    crossProduct(:,i) = crossProduct(:,i)/norm(crossProduct(:,i));
-    if mod(i,1000) == 0
-        disp(i);
-    end
-end
-matlabpool close;
+%USE intersect function
+%volElementIdx = ceil(find(ismember(faces',volSurface,'rows'))/4);
+%calculate cross product analytically
+crossProduct = zeros(3, size(volSurface,2));
+crossProduct(1,:) = u(2,:).*v(3,:) - u(3,:).*v(2,:);
+crossProduct(2,:) = u(3,:).*v(1,:) - u(1,:).*v(3,:);
+crossProduct(3,:) = u(1,:).*v(2,:) - u(2,:).*v(1,:);
+crossProduct = normc(crossProduct);
+
+%mapping from triangles to elements, combined with sign multiplication to
+%make sure the direction is towards outside of the gm.
 
 %We will need to make sure the direction is outwards by following script.
-surfElemC = elemCenter(node,elem(:,volElementIdx)); %tetra centers.
-surfTriC = elemCenter(node,volSurface'); %triangle centers.
+surfElemC = elemCenter(node,elem(:,mapping)); %tetra centers.
+surfTriC = elemCenter(node,volSurface); %triangle centers.
 
-%Convert the direction if it is inwards. 
+%Convert the direction if it is inwards.
 surfNormal = crossProduct .* repmat(sign(sum(crossProduct .* (surfTriC-surfElemC))),3,1);
-surfElements.elemCenters = surfTriC;
-surfElements.elemIndices = volElementIdx;
+surfElements.triCenters = surfTriC;
+surfElements.elemIndices = mapping;
+surfElements.tetCenters = surfElemC;
