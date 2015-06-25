dcb173aee7e98cf7490b3c05c3946058ddb47326
U-CSL-PC\Seyhmus <Seyhmus@CSL-PC.(none)>
2015-06-25 13:29:08 -0400

    re-implementation of Ruffini et al.'s method

---
+++ RuffiniEtAl/findQuadraticLinearTerms4Objective.m
@@ -0,0 +1,83 @@
+function [Q,b] = findQuadraticLinearTerms4Objective(mapV2E, T, roi, surfNormal, tmap, tMin, E0) 
+%Finds quadratic and linear terms for the objective function 
+%
+%
+% Synopsis: [Q,b] = findQuadraticLinearTerms4Objective(headMesh, mapV2E, ...
+%                       roi, surfaceNormal)
+%
+%
+% Input:    headMesh        = tetrahedral head mesh.
+%               .cell       = tetrahedra definition matrix. Size: 4 x #cells
+%               .node       = node coordinates.
+%           mapV2E          = mapping from potential to electric field
+%           T               = transfer matrix
+%           roi             = region of interest
+%           surfaceNormal   = surface normal for the ROI elements.
+%           tmap            = tmap values for the ROI elements.
+%           E0              = desired electric field.
+%
+%
+% Output:   Q           = quadratic term in the objective
+%           b           = linear coefficients in the objective
+%
+
+M = size(mapV2E,1)/3; % # elements
+N = size(mapV2E,2); % # nodes
+L = size(T,2); % # electrodes
+
+if size(T,1) ~= N
+    error('mismatch in transfer matrix size');
+end
+if size(roi,2) ~= M
+    roi = roi';
+end
+
+if (size(surfNormal,2) == M)
+    surfNormal = surfNormal(:,roi==1);
+end
+
+if (numel(tmap) == M)
+    tmap = tmap(roi==1);
+end
+
+if isempty(tMin)
+    tMin = 2;
+end
+
+if numel(E0) == M
+    E0 = E0(roi==1);
+end
+    
+
+%% Calculate mapping matrices
+
+tmap(abs(tmap)<tMin) = 0; %thresholding tmap
+W = abs(tmap);
+
+roiIdx = find(roi==1);
+eRoiIdx = sort([3*roiIdx 3*roiIdx-1 3*roiIdx-2]);
+
+surfNormalInCells = mat2cell(surfNormal',ones(size(surfNormal,2),1),3);
+mapOntoSurfNormal = sparse(blkdiag(surfNormalInCells{:}));
+
+%normal component of electric field (tmap weighted) 
+Ew = (diag(W) * mapOntoSurfNormal * mapV2E(eRoiIdx,:)) * T;
+
+%desired normal component of electric field (tmap weighted)
+Yw = diag(tmap) * E0; %Yw
+
+k = numel(W) / sum(W.^2);
+
+Q = k * (Ew' * Ew) ;
+b = k * (Yw' * Ew)';
+
+
+
+
+
+
+
+
+
+
+
---
+++ RuffiniEtAl/weightedLeastSquaresTotalIndividual.m
@@ -0,0 +1,107 @@
+function [I, fVal, dV] = weightedLeastSquaresTotalIndividual(Q, b, tot, ind)
+% Finds hd-tDCS electrode current stimulus pattern using weighted least squares
+% with total and individual electrode currents.
+%
+%
+% Synopsis: [I, fval, dV] = weightedLeastSquaresTotalIndividual( ...
+%           sqrtQ_weighted, v_weighted, tot, ind)
+%
+% Input:    sqrtQ_weighted  = chol factor of weighted quadratic matrix.
+%           v_weighted      = weighted right hand side vector.
+%           tot             = total applied current bound.
+%           ind             = individual electrode current bound.
+%
+% Output:   I       =   array of electrode currents.
+%           fVal    =   least squares error for best solution. 
+%           dV      =   dual variables for the constraints.
+
+% Notes:    1. This implementation is to mimic the results in the paper 
+%              'Optimization of multifocal transcranial current stimulation
+%              for weighted cortical pattern targeting from realistic 
+%              modeling of electric fields' by Ruffini et al., 2014.
+%
+%           2. We convert quadratic objective function to norm 2 in order
+%           to speed up the convergence. This is the reason behind using
+%           Cholesky factor of the quadratic matrix instead of the matrix
+%           itself. minimize(x' * Q * x + b' * x) is equivalent to
+%           minimize( norm(chol(Q) * (x - m), 2 ) ) where m = Q\v.
+
+tic;
+L = size(Q,2); %number of electrodes
+sqrtQ = chol(Q,p);
+if p>0
+    sqrtQ = chol(Q+1e-9*eye(size(Q)));
+end
+m = 2*Q'\b;
+
+if size(ind,1) == L
+    ind(L+1,:) = ind(end,:);
+end
+
+cvx_begin quiet
+cvx_solver sedumi
+cvx_precision high
+variable x(L)
+expression y(L+1)
+y = [x; -sum(x)];
+
+dual variable totConstV
+dual variable indConstLB
+dual variable indConstUB
+
+minimize norm(sqrtQ * (x+m));
+subject to
+totConstV : norm(y,1) <= 2*tot;
+indConstLB : -ind <= y;
+indConstUB : y <= ind;
+cvx_end
+
+if ~strcmp(cvx_status,'Solved')
+    fprintf('%s\n','No solution with high precision, trying low precision.');
+    
+    cvx_begin quiet
+    cvx_solver sedumi
+    cvx_precision low
+    variable x(L)
+    expression y(L+1)
+    y = [x; -sum(x)];
+    
+    dual variable totConstV
+    dual variable indConstLB
+    dual variable indConstUB
+    
+    minimize norm(sqrtQ * (x+m));
+    subject to
+    totConstV : norm(y,1) <= 2*tot;
+    indConstLB : -ind <= y;
+    indConstUB : y <= ind;
+    cvx_end
+end
+
+if ~strcmp(cvx_status,'Solved')
+    fprintf('%s\n','No solution with sedumi, trying sdpt3.');
+    
+    cvx_begin quiet
+    cvx_solver SDPT3
+    variable x(L)
+    expression y(L+1)
+    y = [x; -sum(x)];
+    
+    dual variable totConstV
+    dual variable indConstLB
+    dual variable indConstUB
+    
+    minimize norm(sqrtQ * (x+m));
+    subject to
+    totConstV : norm(y,1) <= 2*tot;
+    indConstLB : -ind <= y;
+    indConstUB : y <= ind;
+    cvx_end
+end
+
+I = x;
+fVal = cvx_optval;
+dV = [totConstV; indConstLB; indConstUB];
+fprintf('%s%f%s\n', 'Weighted least squares solution is found in ', toc, ...
+    ' seconds.');
+end
